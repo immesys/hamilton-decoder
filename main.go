ee96a1620b1916f80e8355bd3c60adfadf86f194
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-10-13 11:55:29 -0700

    v1

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/immesys/spawnpoint/spawnable"
+	"gopkg.in/immesys/bw2bind.v5"
+)
+
+type message struct {
+	Srcmac  string `msgpack:"srcmac"`
+	Srcip   string `msgpack:"srcip"`
+	Popid   string `msgpack:"popid"`
+	Poptime int64  `msgpack:"poptime"`
+	Brtime  int64  `msgpack:"brtime"`
+	Rssi    int    `msgpack:"rssi"`
+	Lqi     int    `msgpack:"lqi"`
+	Payload []byte `msgpack:"payload"`
+}
+
+type TypeHandler interface {
+	Init(cl *bw2bind.BW2Client, p *spawnable.Params)
+	Handle(m *bw2bind.SimpleMessage)
+}
+
+var Handlers map[uint16]TypeHandler
+var c_forwarded uint64
+var c_dropped uint64
+
+func Register(t uint16, h TypeHandler) {
+	if Handlers == nil {
+		Handlers = make(map[uint16]TypeHandler)
+	}
+	Handlers[t] = h
+}
+func main() {
+	cl := bw2bind.ConnectOrExit("")
+	cl.SetEntityFromEnvironOrExit()
+	params := spawnable.GetParamsOrExit()
+	signal := params.MustString("signal")
+	listenuri := params.MustString("listenuri")
+	if !strings.HasSuffix(listenuri, "/") {
+		listenuri += "/"
+	}
+	// Initialize the handlers
+	for _, h := range Handlers {
+		h.Init(cl, params)
+	}
+	ch := cl.SubscribeOrExit(&bw2bind.SubscribeParams{
+		AutoChain: true,
+		URI:       listenuri + "*/s.hamilton/+/i.l7g/signal/" + signal,
+	})
+	for i := 0; i < 10; i++ {
+		go handleIncoming(ch)
+	}
+	PrintStats()
+}
+
+func PrintStats() {
+	for {
+		time.Sleep(5 * time.Second)
+		fmt.Printf("forwarded=%d\n", c_forwarded)
+	}
+}
+
+func handleIncoming(ch chan *bw2bind.SimpleMessage) {
+	for m := range ch {
+		po := m.GetOnePODF("2.0.10.1")
+		if po == nil {
+			fmt.Printf("po mismatch\n")
+			continue
+		}
+		im := message{}
+		po.(bw2bind.MsgPackPayloadObject).ValueInto(&im)
+		if len(im.Payload) < 2 {
+			atomic.AddUint64(&c_dropped, 1)
+			continue
+		}
+		mtype := binary.LittleEndian.Uint16(im.Payload)
+		fmt.Printf("Type was %d\n", mtype)
+		h, ok := Handlers[mtype]
+		if ok {
+			h.Handle(m)
+		} else {
+			fmt.Printf("no handler found\n")
+			atomic.AddUint64(&c_dropped, 1)
+		}
+	}
+}
---
+++ type4_hamiltonTemperature.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strings"
+	"sync/atomic"
+
+	"github.com/immesys/spawnpoint/spawnable"
+	"github.com/pborman/uuid"
+	"gopkg.in/immesys/bw2bind.v5"
+)
+
+func init() {
+	Register(4, &Type4Handler{})
+}
+
+var Type4Namespace = uuid.Parse("821c0592-9316-4716-b4f4-d2c0dc436dab")
+
+type Type4Handler struct {
+	cl      *bw2bind.BW2Client
+	p       *spawnable.Params
+	baseuri string
+}
+
+type T4Message struct {
+	UUID  string  `msgpack:"UUID"`
+	Time  int64   `msgpack:"Time"`
+	Value float64 `msgpack:"Value"`
+}
+
+func (t4 *Type4Handler) Init(cl *bw2bind.BW2Client, p *spawnable.Params) {
+	t4.cl = cl
+	t4.p = p
+	t4.baseuri = p.MustString("type4_base")
+	if !strings.HasSuffix(t4.baseuri, "/") {
+		t4.baseuri += "/"
+	}
+}
+func (t4 *Type4Handler) Handle(m *bw2bind.SimpleMessage) {
+	fmt.Printf("handling message\n")
+	po := m.GetOnePODF("2.0.10.1")
+	if po == nil {
+		fmt.Printf("po mismatch\n")
+		return
+	}
+	im := message{}
+	po.(bw2bind.MsgPackPayloadObject).ValueInto(&im)
+	if len(im.Payload) < 2 {
+		atomic.AddUint64(&c_dropped, 1)
+		return
+	}
+	tempi := binary.LittleEndian.Uint32(im.Payload[9:])
+	tempf := float64(tempi) / 10000.
+	obj := T4Message{
+		Time:  im.Brtime,
+		Value: tempf,
+		UUID:  uuid.NewSHA1(Type4Namespace, []byte(im.Srcmac)).String(),
+	}
+	npo, _ := bw2bind.CreateMsgPackPayloadObject(bw2bind.PONumTimeseriesReading, obj)
+	err := t4.cl.Publish(&bw2bind.PublishParams{
+		URI:            fmt.Sprintf("%ss.hamilton/%s/i.temperature/signal/temperature", t4.baseuri, im.Srcmac),
+		PayloadObjects: []bw2bind.PayloadObject{npo},
+		AutoChain:      true,
+	})
+	if err != nil {
+		fmt.Printf("failed to publish: %v\n", err)
+	}
+}
