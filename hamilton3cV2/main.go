53c5ef1fb212537716c4e1a159c3e3808d2c6dbb
Michael Andersen <michael@steelcode.com>
2017-04-06 10:04:43 -0700

    Add full decoding logic

--- hamilton3cV2/main.go
+++ hamilton3cV2/main.go
@@ -6,6 +6,7 @@ import (
 	"crypto/cipher"
 	"encoding/binary"
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/immesys/hamilton-decoder/common"
@@ -117,83 +118,95 @@ func (t *Handler) Handle(sm *bw2bind.SimpleMessage, im *common.Message) {
 		}
 	}
 
-	flags := binary.LittleEndian.Uint16
-	fmt.Printf("Payload %x\n", plaintext)
-	_ = dce
-	return
-	/*
-		flags := binary.LittleEndian.Uint16(im.Payload[2:])
-		dat := make(map[string]interface{})
-		if flags&0x01 != 0 { //tmp_die
-			raw := binary.LittleEndian.Uint16(im.Payload[4:])
-			dat["tp_die_temp"] = float64(int16(raw)>>2) * 0.03125
-		}
-		if flags&0x02 != 0 { //tmp_val
-			raw := binary.LittleEndian.Uint16(im.Payload[6:])
-			uv := float64(int16(raw)) * 0.15625
-			dat["tp_voltage"] = uv
-			fmt.Printf("voltage was %f uv\n", uv)
-			v := uv / 1000000.0
-			die_temp := dat["tp_die_temp"].(float64) + 273.15
-			tref := 298.15 //K
-			S_0 := 6e-14
-			a1 := 1.75e-3
-			a2 := -1.678e-5
-			S := S_0 * (1 + a1*(die_temp-tref) + a2*(die_temp-tref)*(die_temp-tref))
-			b0 := -2.94e-5
-			b1 := -5.7e-7
-			b2 := 4.63e-9
-			c2 := 13.4
-			v_os := b0 + b1*(die_temp-tref) + b2*(die_temp-tref)*(die_temp-tref)
-			f_vo := v - v_os + c2*(v-v_os)*(v-v_os)
-			t_obj4 := math.Pow(die_temp, 4) + f_vo/S
-			t_obj := math.Pow(t_obj4, 1.0/4)
-			dat["radiant_temp"] = t_obj
-			fmt.Println("radiant was", t_obj)
-		}
-		if flags&0x04 != 0 { //hdc_tmp
-			raw := binary.LittleEndian.Uint16(im.Payload[8:])
-			fmt.Printf("hdc_tmp raw: %d\n", raw)
-			dat["air_temp"] = (float64(raw)/65536)*165 - 40
-		}
-		if flags&0x08 != 0 { //hdc_rh
-			raw := binary.LittleEndian.Uint16(im.Payload[10:])
-			fmt.Printf("hdc_rh raw: %d\n", raw)
-			dat["air_rh"] = (float64(raw) / 65536) * 100
-		}
-		if flags&0x10 != 0 { //lux
-			raw := binary.LittleEndian.Uint16(im.Payload[12:])
-			fmt.Printf("lux raw: %d\n", raw)
-			dat["lux"] = math.Pow(10, float64(raw)/(65536.0/5.0))
-		}
-		if flags&0x20 != 0 { //buttons
-			raw := binary.LittleEndian.Uint16(im.Payload[14:])
-			fmt.Printf("buttons raw: %d\n", raw)
-			dat["button_events"] = raw
-		}
-		if flags&0x40 != 0 { //uptime
-			dat["uptime"] = binary.LittleEndian.Uint64(im.Payload[16:])
-			fmt.Printf("uptime raw: %d\n", binary.LittleEndian.Uint64(im.Payload[16:]))
-		}
-		if flags&0x80 != 0 { //occupancy
-			dat["presence"] = float64(binary.LittleEndian.Uint16(im.Payload[24:26])) / 32768
-			fmt.Printf("presence raw: %d\n", binary.LittleEndian.Uint16(im.Payload[24:26]))
-		}
-		if flags&0x07 == 0x07 { //we can do operative temperature
-			v := 0.2
-			r10v := math.Pow(10*v, 1/2.0)
-			dat["operative_temp"] = (dat["air_temp"].(float64) + dat["air_temp"].(float64)*r10v) / (1 + r10v)
-		}
-		dat["time"] = im.Brtime
-		npo, _ := bw2bind.CreateMsgPackPayloadObject(bw2bind.FromDotForm("2.0.11.2"), dat)
-		err := t6.cl.Publish(&bw2bind.PublishParams{
-			URI:            fmt.Sprintf("%ss.hamilton/%s/i.temperature/signal/operative", t6.baseuri, im.Srcmac),
-			PayloadObjects: []bw2bind.PayloadObject{npo},
-			AutoChain:      true,
-		})
-		if err != nil {
-			fmt.Printf("t6 failed to publish: %v\n", err)
-		} else {
-			fmt.Println("t6 pub")
-		}*/
+	f_uptime := binary.LittleEndian.Uint64(plaintext[0:8])
+	f_flags := binary.LittleEndian.Uint16(plaintext[8:10])
+	f_acc_x := binary.LittleEndian.Uint16(plaintext[10:12])
+	f_acc_y := binary.LittleEndian.Uint16(plaintext[12:14])
+	f_acc_z := binary.LittleEndian.Uint16(plaintext[14:16])
+	f_mag_x := binary.LittleEndian.Uint16(plaintext[16:18])
+	f_mag_y := binary.LittleEndian.Uint16(plaintext[18:20])
+	f_mag_z := binary.LittleEndian.Uint16(plaintext[20:22])
+	f_tmp_die := binary.LittleEndian.Uint16(plaintext[22:24])
+	f_tmp_val := binary.LittleEndian.Uint16(plaintext[24:26])
+	f_hdc_tmp := binary.LittleEndian.Uint16(plaintext[26:28])
+	f_hdc_rh := binary.LittleEndian.Uint16(plaintext[28:30])
+	f_light_lux := binary.LittleEndian.Uint16(plaintext[30:32])
+	f_buttons := binary.LittleEndian.Uint16(plaintext[32:34])
+	f_occup := binary.LittleEndian.Uint16(plaintext[34:36])
+	dat := make(map[string]float64)
+	dat["uptime"] = float64(f_uptime)
+	if f_flags&(1<<0) != 0 {
+		//accel
+		dat["acc_x"] = float64(f_acc_x) * 0.244
+		dat["acc_y"] = float64(f_acc_y) * 0.244
+		dat["acc_z"] = float64(f_acc_z) * 0.244
+
+	}
+	if f_flags&(1<<1) != 0 {
+		dat["mag_x"] = float64(f_mag_x) * 0.1
+		dat["mag_y"] = float64(f_mag_y) * 0.1
+		dat["mag_z"] = float64(f_mag_z) * 0.1
+	}
+	if f_flags&(1<<2) != 0 {
+		//TMP
+		dat["tp_die_temp"] = float64(int16(f_tmp_die)>>2) * 0.03125
+		uv := float64(int16(f_tmp_val)) * 0.15625
+		dat["tp_voltage"] = uv
+		//		fmt.Printf("voltage was %f uv\n", uv)
+		// v := uv / 1000000.0
+		// die_temp := dat["tp_die_temp"].(float64) + 273.15
+		// tref := 298.15 //K
+		// S_0 := 6e-14
+		// a1 := 1.75e-3
+		// a2 := -1.678e-5
+		// S := S_0 * (1 + a1*(die_temp-tref) + a2*(die_temp-tref)*(die_temp-tref))
+		// b0 := -2.94e-5
+		// b1 := -5.7e-7
+		// b2 := 4.63e-9
+		// c2 := 13.4
+		// v_os := b0 + b1*(die_temp-tref) + b2*(die_temp-tref)*(die_temp-tref)
+		// f_vo := v - v_os + c2*(v-v_os)*(v-v_os)
+		// t_obj4 := math.Pow(die_temp, 4) + f_vo/S
+		// t_obj := math.Pow(t_obj4, 1.0/4)
+		// dat["radiant_temp"] = t_obj
+		// fmt.Println("radiant was", t_obj)
+	}
+
+	if f_flags&(1<<3) != 0 {
+		//HDC
+		rh := (float64(f_hdc_rh) / 65536) * 100
+		t := (float64(f_hdc_tmp)/65536)*165 - 40
+		dat["air_temp"] = t
+		dat["air_rh"] = rh
+		expn := (17.67 * t) / (t + 243.5)
+		dat["air_hum"] = (6.112 * math.Pow(math.E, expn) * rh * 2.1674) / (273.15 + t)
+	}
+	if f_flags&(1<<4) != 0 {
+		//LUX
+		dat["lux"] = math.Pow(10, float64(f_light_lux)/(65536.0/5.0))
+	}
+	if f_flags&(1<<5) != 0 {
+		dat["button_events"] = float64(f_buttons)
+	}
+	if f_flags&(1<<6) != 0 {
+		dat["presence"] = float64(f_occup) / 32768
+	}
+
+	// if flags&0x07 == 0x07 { //we can do operative temperature
+	// 	v := 0.2
+	// 	r10v := math.Pow(10*v, 1/2.0)
+	// 	dat["operative_temp"] = (dat["air_temp"].(float64) + dat["air_temp"].(float64)*r10v) / (1 + r10v)
+	// }
+	dat["time"] = float64(im.Brtime)
+	npo, _ := bw2bind.CreateMsgPackPayloadObject(bw2bind.FromDotForm("2.0.11.2"), dat)
+	err = t.cl.Publish(&bw2bind.PublishParams{
+		URI:            fmt.Sprintf("%ss.hamilton/%s/i.temperature/signal/operative", t.baseuri, im.Srcmac),
+		PayloadObjects: []bw2bind.PayloadObject{npo},
+		AutoChain:      true,
+	})
+	if err != nil {
+		fmt.Printf("tcrypt failed to publish: %v\n", err)
+	} else {
+		fmt.Println("tcrypt pub")
+	}
 }
